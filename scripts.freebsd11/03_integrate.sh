15e1d246beb57bea46032e7a44fbcbf5d06e54fb
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-02-10 07:13:36 +0100

    textual descrption of integration

--- scripts.freebsd11/03_integrate.sh
+++ scripts.freebsd11/03_integrate.sh
@@ -1,5 +1,29 @@
 #!/bin/csh
 
+#
+# we need to allow dnssafety user to bind to 53 port (TCP, UDP)
+# see:
+#     https://kb.isc.org/docs/aa-00621
+#     https://www.freebsd.org/doc/en_US.ISO8859-1/books/handbook/mac-policies.html
+#
+echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
+echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
+echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
+echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
+echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
+#
+# /boot/loader.conf:
+# mac_portacl_load="YES"
+#
+# /etc/sysctl.conf:  
+# net.inet.ip.portrange.reservedlow=0
+# net.inet.ip.portrange.reservedhigh=0
+# security.mac.portacl.port_high=1023
+# security.mac.portacl.suser_exempt=1
+# security.mac.portacl.rules=uid:1005:tcp:53,uid:1005:udp:53
+#
+# NOTE!!! the 1005 is the uid of dnssafety user!
+#
 # relabel folder
 chown -R dnssafety:dnssafety /opt/dnssafety
 

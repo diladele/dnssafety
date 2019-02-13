8c32fc6d7111d2d62f7ef962c33ccb8bc3a5c4a8
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-02-13 06:21:19 +0100

    updated

--- scripts.freebsd11/01_update.sh
+++ scripts.freebsd11/01_update.sh
@@ -7,9 +7,5 @@ freebsd-update install
 # bootstrap pkg
 env ASSUME_ALWAYS_YES=YES pkg bootstrap
 
-# get new ports
-# portsnap fetch
-# portsnap extract
-
 # and reboot
-# reboot
+reboot
--- scripts.freebsd11/03_integrate.sh
+++ scripts.freebsd11/03_integrate.sh
@@ -6,11 +6,6 @@
 #     https://kb.isc.org/docs/aa-00621
 #     https://www.freebsd.org/doc/en_US.ISO8859-1/books/handbook/mac-policies.html
 #
-echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
-echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
-echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
-echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
-echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
 #
 # /boot/loader.conf:
 # mac_portacl_load="YES"
@@ -20,11 +15,9 @@ echo "IMPORTANT - MANUAL CHANGE REQUIRED!"
 # net.inet.ip.portrange.reservedhigh=0
 # security.mac.portacl.port_high=1023
 # security.mac.portacl.suser_exempt=1
-# security.mac.portacl.rules=uid:1005:tcp:53,uid:1005:udp:53
+# security.mac.portacl.rules=uid:53:tcp:53,uid:53:udp:53
 #
-# NOTE!!! the 1005 is the uid of dnssafety user!
-#
-# relabel folder (note we reuse the bind user here!)
+# finally relabel folder (note we reuse the bind user here!)
 chown -R bind:bind /opt/dnssafety
 
 # restart all daemons

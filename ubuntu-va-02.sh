06907a6a2f3a6bfb1d4763b25696fe98e707e814
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-04-05 11:19:34 +0200

    helper

---
+++ ubuntu-va-02.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# check we are root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# install dns safety
+pushd scripts.ubuntu18
+bash 02_dnssafety.sh && bash 03_integrate.sh 
+popd
+
+# install dns UI
+pushd scripts.ui
+bash 01_update.sh && \
+bash 02_apache.sh && \
+bash 03_dnssafety-ui.sh && \
+bash 04_integrate.sh
+popd
+
+# install va
+pushd scripts.va
+bash 01_login.sh && bash 02_harden.sh
+popd
+
+# tell 
+echo "SUCCESS"
+echo "SUCCESS"
+echo "SUCCESS --- VA is Ready (check the license and publish it) ---"
+cat /opt/dnssafety/etc/license.pem | grep "Not After"
+echo "SUCCESS"
+echo "SUCCESS"

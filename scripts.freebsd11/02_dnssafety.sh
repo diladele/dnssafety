419dd512e7709765e8c08b245aa234d422d3a068
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-08-10 16:52:36 +0200

    release 0.12

--- scripts.freebsd11/02_dnssafety.sh
+++ scripts.freebsd11/02_dnssafety.sh
@@ -3,7 +3,7 @@
 # setup some configuration variables
 ARCH=`uname -m`
 MAJOR=0.12.0
-MINOR=C1E5
+MINOR=0DD5
 
 # get latest version of dns safety
 fetch http://packages.diladele.com/dnssafety/$MAJOR.$MINOR/$ARCH/release/freebsd11/dnssafety-$MAJOR-$ARCH.txz

8f13256ae8dcb1021f3ef064076d4e121c07df1e
Rafael Akchurin <rafael.akchurin@diladele.com>
2019-12-31 16:01:20 +0100

    change password script as required by AWS marketplace

--- appliance/aws/chpass_aws.sh
+++ appliance/aws/chpass_aws.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# update root password in the Web Safety to a random value
+# update root password in the Dns Safety to a random value
 # required for vm publication in the AWS Cloud Marketplace
 #
 
-FLAG_FILE="/opt/websafety/etc/password_reset.flag"
+FLAG_FILE="/opt/dnssafety/etc/password_reset.flag"
 
 if [ -f $FLAG_FILE ]; then
 
@@ -17,10 +17,10 @@ else
     NEWPASW=`curl http://169.254.169.254/latest/meta-data/instance-id`
 
     # update the password in the database 
-    sudo -u websafety python3 /opt/websafety-ui/var/console/reset_password.py --password=$NEWPASW
+    sudo -u daemon python3 /opt/dnssafety-ui/var/console/reset_password.py --password=$NEWPASW
 
-    # change the template too so that user known what shall be used as password
-    sudo -u websafety sed -i "s/Passw0rd/InstanceID/g" /opt/websafety-ui/var/console/frame/templates/login.html
+    # change the template too so that user knows what shall be used as password
+    sudo -u daemon sed -i "s/Passw0rd/InstanceID/g" /opt/dnssafety-ui/var/console/frame/templates/login.html
 
     # raise the updated password flag so that we do not regenerate it next reboot
     echo "Do NOT remove this file!" >  "$FLAG_FILE"
@@ -30,8 +30,8 @@ else
     echo "" >> "$FLAG_FILE"
 
     echo "If you remove this flag file, the /etc/systemd/service/chpass_aws.service service "  >> "$FLAG_FILE"
-    echo "will automatically call /opt/websafety/bin/chpass_aws.sh file that will reset"       >> "$FLAG_FILE"
-    echo "the password of the root user in Web Safety UI to ID of this instance."              >> "$FLAG_FILE"
+    echo "will automatically call /opt/dnssafety/bin/chpass_aws.sh file that will reset"       >> "$FLAG_FILE"
+    echo "the password of the root user in Dns Safety UI to ID of this instance."              >> "$FLAG_FILE"
     echo "This is required for VMs generated from AMI template in Amazon AWS."                 >> "$FLAG_FILE"
     echo "" >> "$FLAG_FILE"
     echo "" >> "$FLAG_FILE"

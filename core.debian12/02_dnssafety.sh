78fbced2d5cd5b950ab9ca5d21afba6a4dd06acf
Rafael Akchurin <rafael.akchurin@diladele.com>
2023-11-15 11:08:55 +0100

    2.0

--- core.debian12/02_dnssafety.sh
+++ core.debian12/02_dnssafety.sh
@@ -7,8 +7,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # default arc
-MAJOR="1.0.0"
-MINOR="15F2"
+MAJOR="2.0.0"
+MINOR="B875"
 ARCH="amd64"
 
 # see if it is RPI or not?
--- ui.deb/02_ui.sh
+++ ui.deb/02_ui.sh
@@ -7,8 +7,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # default arc
-MAJOR="1.0.0"
-MINOR="7112"
+MAJOR="2.0.0"
+MINOR="48D8"
 ARCH="amd64"
 
 # see if it is RPI or not?

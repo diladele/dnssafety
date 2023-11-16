84769ef17b1979b5c956db9418e0107e3f014988
Rafael Akchurin <rafael.akchurin@diladele.com>
2023-11-16 14:29:23 +0100

    2.0

--- core.debian12/02_dnssafety.sh
+++ core.debian12/02_dnssafety.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="2.0.0"
-MINOR="CBFD"
+MINOR="DB9A"
 ARCH="amd64"
 
 # see if it is RPI or not?
--- ui.deb/02_ui.sh
+++ ui.deb/02_ui.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="2.0.0"
-MINOR="D545"
+MINOR="22FC"
 ARCH="amd64"
 
 # see if it is RPI or not?

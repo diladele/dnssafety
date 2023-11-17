8f46ba3f31355bf206a74d8840628993547c3798
Rafael Akchurin <rafael.akchurin@diladele.com>
2023-11-17 13:42:54 +0100

    disable systemd resolver on azure image

--- appliance/azure/01_adjust.sh
+++ appliance/azure/01_adjust.sh
@@ -6,5 +6,11 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# in debian 12 on azure systemd resolver is active, we need to disable it
+# failure to do so will mean dsdnsd daemon will not be able to start as
+# port 53 would be taken by the systemd resolver
+systemctl disable --now systemd-resolved
+systemctl stop systemd-resolved
+
 # the Azure deployment insists on this
 sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config

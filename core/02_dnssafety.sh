99f7338246d1397729f3faeae0460a81de17671f
Rafael Akchurin <rafael.akchurin@diladele.com>
2024-10-24 13:56:40 +0200

    2.1

--- core/02_dnssafety.sh
+++ core/02_dnssafety.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="2.1.0"
-MINOR="07A5"
+MINOR="3BB1"
 ARCH="amd64"
 
 # see if it is RPI or not?
--- ui/02_ui.sh
+++ ui/02_ui.sh
@@ -8,7 +8,7 @@ fi
 
 # default arc
 MAJOR="2.1.0"
-MINOR="12D7"
+MINOR="6974"
 ARCH="amd64"
 
 # see if it is RPI or not?

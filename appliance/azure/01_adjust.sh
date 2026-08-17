8f49f6987b78592eaa1a7b65fb5c3db1a9425f7a
Rafael Akchurin <rafael.akchurin@diladele.com>
2026-08-17 08:14:07 +0200

    updated

--- appliance/azure/01_adjust.sh
+++ appliance/azure/01_adjust.sh
@@ -12,22 +12,26 @@ fi
 systemctl disable --now systemd-resolved
 systemctl stop systemd-resolved
 
-#
-# re-enable extensions https://wiki.debian.org/Cloud/MicrosoftAzure
-#
-sed -i -e 's/^Extensions\.Enabled=.*$/Extensions.Enabled=y/' /etc/waagent.conf
-sed -i -e 's/^AutoUpdate\.Enabled=.*$/AutoUpdate.Enabled=y/' /etc/waagent.conf
+## not needed after january 2025
+##
+## re-enable extensions https://wiki.debian.org/Cloud/MicrosoftAzure
+##
+#sed -i -e 's/^Extensions\.Enabled=.*$/Extensions.Enabled=y/' /etc/waagent.conf
+#sed -i -e 's/^AutoUpdate\.Enabled=.*$/AutoUpdate.Enabled=y/' /etc/waagent.conf
 
 # disable network management from Admin UI on Azure
 patch /opt/dnssafety-ui/var/console/node/models.py < models.py.patch
 
+# install tools for TPM certification
+apt install -y tpm2-tools
+
 #
 # enable login with older ssh keys (required for Azure Certification Tools)
 #
-cat <<EOT >> /etc/ssh/sshd_config.d/51-dnssafety.conf
-HostKeyAlgorithms=ssh-rsa,ssh-rsa-cert-v01@openssh.com
-PubkeyAcceptedAlgorithms=+ssh-rsa,ssh-rsa-cert-v01@openssh.com
-EOT
+#cat <<EOT >> /etc/ssh/sshd_config.d/51-dnssafety.conf
+#HostKeyAlgorithms=ssh-rsa,ssh-rsa-cert-v01@openssh.com
+#PubkeyAcceptedAlgorithms=+ssh-rsa,ssh-rsa-cert-v01@openssh.com
+#EOT
 
 # the Azure deployment insists on this
 sed -i 's/ClientAliveInterval 120/ClientAliveInterval 180/g' /etc/ssh/sshd_config
